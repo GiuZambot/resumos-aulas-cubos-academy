9381d4f98e9408d0a07e406ceb1ec136e80c7dac
Giu Zambot <giuzambot@gmail.com>
2021-06-18 13:58:39 -0300

    Exerc├¡cios entregues

--- exercicios-repos/transferir.bat
+++ exercicios-repos/transferir.bat
@@ -1,1 +1,7 @@
 robocopy . D:\reposit\resumos-aulas-cubos-academy\exercicios-repos /e /purge  /xd .git /xd .vscode /xf .gitignore
+cd D:\reposit\resumos-aulas-cubos-academy
+git status
+git add .
+git commit -m "Exercícios entregues"
+git push
+pause
