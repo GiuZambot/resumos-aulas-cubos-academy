1179f1a035c559b887b9a9134232905acd3df2d2
Giu Zambot <giuzambot@gmail.com>
2021-06-19 18:45:26 -0300

    PhP

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-<?php include_once("index2.html");
-
-
-$listDiretorio = array_diff(
-    scandir('/produtos/'),
-    ['.', '..']
-);
-
-//Percorremos todos os itens da nossa lista
-foreach($listDiretorio as $diretorio){
-    $isDir = is_dir(''/produtos/' . $diretorio) ? 'Diretório' : 'Arquivo';
-
-    echo "<p>{$diretorio} - {$isDir}</p>";
-}
-
-?>
+<?php include_once("index.html");?>
