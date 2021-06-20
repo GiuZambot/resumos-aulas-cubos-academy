609983420daea9bf5f7d4032b3918902cdea277c
Giu Zambot <giuzambot@gmail.com>
2021-06-20 14:23:37 -0300

    Novos arquivos para o menu automatico

--- criar-menus.bat
+++ criar-menus.bat
@@ -1,33 +1,17 @@
-set arq="teste.html"
+CHCP 65001
+SET arq="menu.json"
+SET dia=%date:/=-%
 
-echo "<p>Atualizado: em %date%</p>" > %arq%
+echo { "status": [ { "atualizado" : "%dia%" } ], "menu": [ > %arq%
 
-SET atual="<p>Atualizado: em</p>"
-
-SET Quoted="{Atualizado: ()em"
-
-FOR /F "delims=" %%I IN (%Quoted%) DO SET Unquoted=%%I
-
-ECHO %Unquoted% >> %arq%
-
-@REM for /f "tokens=1,2,3 delims=/" %%a in ("%date%") do (echo Ano - %%c Mes - %%b Dia - %a)
-
-@REM set /P teste=promptString
-@REM echo "%teste%" >> teste.html
-
-@REM if %teste% equ 1 (echo "Deu um" >> %arq%)
-
-@REM for %%x in (a,b,c) do echo %%x >> %arq%
-
-@REM for /d /r %%a in (*) do (echo %%a >> %arq%)
-
-@REM for /d %%a in (*) do (echo "<li onclick=jax('%%a/index')> %%a </li>" >> %arq% | dir "./%%a" >> %arq%)
-
-set ini="<li onclick=jax"
-
-@REM for /d %%a in (*) do (echo %ini%%%a >> %arq% | for /d %%b in (%%a/*) do (echo %%b >> %arq% | for /d %%c in (%%a/%%b/*) do (echo %%c >> %arq%) )
+for /d %%a in (*) do (
+    echo { "MenuNome": "%%a"  >> %arq% | for /d %%b in (%%a/*) do (
+        @REM echo %%b%%brf >> %arq% | for /d %%c in (%%a/%%b/*) do (echo %%c >> %arq%) 
+    )
+    echo }, >> %arq%
 )
 
-@REM for /d %%b in (*) do (echo "%%b" >> %arq% )
+ECHO ] } >> %arq%
 
-@REM FOR /L %%i IN (1,1,5) DO (ECHO %%i >> %arq%)
+git add men.json
+git commit -m "Testesdo criador de menu %dia%"
