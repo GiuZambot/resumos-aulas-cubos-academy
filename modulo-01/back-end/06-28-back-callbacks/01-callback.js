6be81d48962d9786d6712721aef62281dc68b488
Giu Zambot <giuzambot@gmail.com>
2021-06-28 20:24:12 -0300

    Adicionado line reader

--- modulo-01/back-end/06-28-back-callbacks/01-callback.js
+++ modulo-01/back-end/06-28-back-callbacks/01-callback.js
@@ -1,11 +1,36 @@
 const fs = require('fs');
 
 // // lendo arquivo
-// console.log("Antes de ler arquivo");
 
 // fs.readFile("./teste.txt", (err, data) => {
 //     console.log(data.toString());
 // });
+
+const lineReader = require('line-reader');
+lineReader.eachLine('./teste.txt', function (line, last) {
+    console.log(line);
+    return false;
+});
+
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//     input: fs.createReadStream('./teste.txt'),
+//     output: process.stdout,
+//     terminal: false
+// })
+
+// rl.on('line', function (line) {
+//     console.log(line) // aqui podes fazer o que precisas com cada linha
+// })
+
+// fs.readFile('./teste.txt', 'utf-8', function (err, data) {
+//     const linhas = data.split(/\r?\n/);
+
+//     linhas.forEach(function (linha) {
+//         console.log(linha); // aqui podes fazer o que precisas com cada linha
+//     })
+// })
 
 // console.log("Depois da chamada, mas executado antes");
 
@@ -66,43 +91,43 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // indexOfTxt != -1 ? console.log(indexOfTxt) : console.log("Não econtrado");
 
-const pessoa = [
-    {
-        nome: "José",
-        idade: 30,
-        altura: 173
-    },
-    {
-        nome: "Pedro",
-        idade: 12,
-        altura: 144
-    },
-    {
-        nome: "Maria",
-        idade: 38,
-        altura: 156
-    }
-];
+// const pessoa = [
+//     {
+//         nome: "José",
+//         idade: 30,
+//         altura: 173
+//     },
+//     {
+//         nome: "Pedro",
+//         idade: 12,
+//         altura: 144
+//     },
+//     {
+//         nome: "Maria",
+//         idade: 38,
+//         altura: 156
+//     }
+// ];
 
-//criando nova array modificada
+// //criando nova array modificada
 
-const arrModif = pessoa.map(x => {
-    return {
-        nome: x.nome,
-        idade: `${x.idade} anos`,
-        altura: `${x.altura / 100}m`,
-        maioridade: x.idade >= 18 ? "É maior de idade" : "É menor de idade"
-    }
-})
+// const arrModif = pessoa.map(x => {
+//     return {
+//         nome: x.nome,
+//         idade: `${x.idade} anos`,
+//         altura: `${x.altura / 100}m`,
+//         maioridade: x.idade >= 18 ? "É maior de idade" : "É menor de idade"
+//     }
+// })
 
-console.log(arrModif);
+// console.log(arrModif);
 
 
-// transformando a array original
-pessoa.map(x => {
-    x.maioridade = x.idade >= 18 ? "É maior de idade" : "É menor de idade";
-    x.idade = `${x.idade} anos`;
-    x.altura = `${x.altura / 100}m`;
-});
+// // transformando a array original
+// pessoa.map(x => {
+//     x.maioridade = x.idade >= 18 ? "É maior de idade" : "É menor de idade";
+//     x.idade = `${x.idade} anos`;
+//     x.altura = `${x.altura / 100}m`;
+// });
 
-console.log(pessoa);
+// console.log(pessoa);
